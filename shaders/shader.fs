6370509dd188e662183825a4ae05918a6d506e6f
xukq <xu_kangqi@mail.dlut.edu.cn>
2022-09-08 16:08:41 +0800

    try texutre method CLAMP_TO_EDGE & REPEAT

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -7,5 +7,5 @@ in vec2 TexCoord;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 void main() {
-	fragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1.0 - TexCoord.x, TexCoord.y)), 0.2);
+	fragColor = mix(texture(texture1, TexCoord * 2), texture(texture2, TexCoord * 2), 0.2);
 }
