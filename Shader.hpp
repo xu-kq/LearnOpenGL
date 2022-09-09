3f3227f51c2666dc8426e22488110f0c3a9096e0
xukq <xu_kangqi@mail.dlut.edu.cn>
2022-09-09 16:46:01 +0800

    Colors: add a light to scene

---
+++ Shader.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include<glad/glad.h>
+
+#include<glm/glm.hpp>
+
+#include<string>
+#include<fstream>
+#include<sstream>
+#include<iostream>
+
+class Shader {
+public:
+	unsigned int ID;
+
+	Shader(const char* vertexPath, const char* fragmentPath);
+
+	void use();
+
+	void setBool(const std::string& name, bool value) const;
+	void setInt(const std::string& name, int value) const;
+	void setFloat(const std::string& name, float value) const;
+	void Shader::setVec3(const std::string& name, const glm::vec3& vec) const;
+	void setMat4(const std::string& name, const glm::mat4&) const;
+};
---
+++ shaders/shader.fs
@@ -0,0 +1,9 @@
+#version 330 core
+out vec4 fragColor;
+
+
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+void main() {
+	fragColor = vec4(objectColor * lightColor, 1.0f);
+}
